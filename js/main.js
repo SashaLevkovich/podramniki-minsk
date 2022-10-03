f6961721c09290a3cd79da259005b072da97bbe5
SashaLevkovich <aleksandr.levkovicha@gmail.com>
2022-10-03 19:56:02 +0300

    change navbar&create anchors&adaptive

--- js/main.js
+++ js/main.js
@@ -1,26 +1,46 @@
-let lastScroll = 0;
-const defaultOffset = 200;
-const header = document.querySelector( '.header' );
+{
+    let lastScroll = 0;
+    const defaultOffset = 200;
+    const header = document.querySelector( '.header' );
 
-const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-const containHide = () => header.classList.contains( 'hide' );
+    const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+    const containHide = () => header.classList.contains( 'hide' );
 
-window.addEventListener( 'scroll', () => {
+    window.addEventListener( 'scroll', () => {
 
-    if ( scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset ) {
-        header.classList.add( 'hide' );
-    } else if ( scrollPosition() < lastScroll && containHide() ) {
-        header.classList.remove( 'hide' );
+        if ( scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset ) {
+            header.classList.add( 'hide' );
+        } else if ( scrollPosition() < lastScroll && containHide() ) {
+            header.classList.remove( 'hide' );
+        }
+
+        lastScroll = scrollPosition();
+
+        if ( lastScroll > 690 ) {
+            header.classList.add( 'fix' );
+        } else if ( lastScroll < 685 ) {
+            header.classList.remove( 'fix' );
+        }
+    } );
+}
+
+{
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
+
+            const blockID = anchor.getAttribute('href').substr(1)
+
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
     }
 
-    lastScroll = scrollPosition();
-
-    if ( lastScroll > 690 ) {
-        header.classList.add( 'fix' );
-    } else if ( lastScroll < 685 ) {
-        header.classList.remove( 'fix' );
-    }
-} );
+}
 
 
 
